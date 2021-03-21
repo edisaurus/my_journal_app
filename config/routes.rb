a200e04462600b5e898cb28b35ab51b0010f0cb7
edisaurus <edswork.ece@gmail.com>
2021-03-22 07:25:03 +0800

    customize registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  devise_for :users
+
+  devise_for :users, controllers: { registrations: "registrations" }
 
   resources :categories do
     resources :tasks
   end
-  
+
 end
