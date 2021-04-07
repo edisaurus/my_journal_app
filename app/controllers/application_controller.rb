c471d15ca278f04b70192d072f547e1a9b012b0b
edisaurus <edswork.ece@gmail.com>
2021-04-07 21:09:55 +0800

    fix tab spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-        def configure_permitted_parameters
-            # perform a sign-up along with the signin data
-            devise_parameter_sanitizer.permit(:sign_in, keys: [:signin])
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, 
-                                                               :last_name, 
-                                                               :email, 
-                                                               :username, 
-                                                               :password, 
-                                                               :password_confirmation])
-        end
+  def configure_permitted_parameters
+    # perform a sign-up along with the signin data
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:signin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, 
+                                                       :last_name, 
+                                                       :email, 
+                                                       :username, 
+                                                       :password, 
+                                                       :password_confirmation])
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,61 +1,60 @@
 class CategoriesController < ApplicationController
-    before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_category, only: %i[ show edit update destroy ]
 
-    def index
-        @categories = current_user.categories
+  def index
+    @categories = current_user.categories
+  end
+
+  def show
+  end
+
+  def new
+    @category = current_user.categories.build
+  end
+
+  def edit
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: "Category created!" }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def show
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to categories_path, notice: "Category updated!" }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def new
-        @category = current_user.categories.build
+  def destroy
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to categories_path, notice: "Category deleted!" }
+      format.json { head :no_content }
     end
+  end
 
-    def edit
-    end
+  private
 
-    def create
-        @category = current_user.categories.build(category_params)
-        respond_to do |format|
-            if @category.save
-                format.html { redirect_to categories_url, notice: "Category created!" }
-                format.json { render :show, status: :created, location: @category }
-            else
-                format.html { render :new, status: :unprocessable_entity }
-                format.json { render json: @category.errors, status: :unprocessable_entity }
-            end
-        end
-    end
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  end
 
-    def update
-        respond_to do |format|
-            if @category.update(category_params)
-                format.html { redirect_to categories_url, notice: "Category updated!" }
-                format.json { render :show, status: :ok, location: @category }
-            else
-                format.html { render :edit, status: :unprocessable_entity }
-                format.json { render json: @category.errors, status: :unprocessable_entity }
-            end
-        end
-    end
-
-    def destroy
-        @category.destroy
-        respond_to do |format|
-            format.html { redirect_to categories_url, notice: "Category deleted!" }
-            format.json { head :no_content }
-        end
-    end
-
-    private
-
-    def set_category
-        @category = current_user.categories.find(params[:id])
-    end
-
-    def category_params
-        params.require(:category).permit(:name, :description, :date_created, :user_id)
-    end
-
+  def category_params
+    params.require(:category).permit(:name, :description, :date_created, :user_id)
+  end
 end
