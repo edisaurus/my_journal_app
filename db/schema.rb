57775c1cda134a859a7a4de70dd6ac830c520e8f
edisaurus <edswork.ece@gmail.com>
2021-04-07 20:13:14 +0800

    add accomlished column to task table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_163235) do
+ActiveRecord::Schema.define(version: 2021_04_07_114355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_163235) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "accomplished", default: false
   end
 
   create_table "users", force: :cascade do |t|
