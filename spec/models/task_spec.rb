615252d5987071b0edcaf4886f923b61f599565d
edisaurus <edswork.ece@gmail.com>
2021-04-10 05:32:40 +0800

    remove test for accomplished attribute

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Task, type: :model do
     it { expect(described_class.new).to validate_presence_of :description }
     it { expect(described_class.new).to validate_presence_of :start_date }
     it { expect(described_class.new).to validate_presence_of :end_date }
-    it { expect(described_class.new).to validate_presence_of :accomplished }
     it { expect(described_class.new).to validate_length_of :description }
     
 end
