072b403fe3e6e2e5561faee3fcb0d9060071fc6e
edisaurus <edswork.ece@gmail.com>
2021-04-10 05:59:42 +0800

    remove accomplished attribute validation presence

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :name, :description, :start_date, :end_date, :accomplished, presence: true
+  validates :name, :description, :start_date, :end_date, presence: true
   validates :description, length: { minimum:10 }
   belongs_to :category
 end
